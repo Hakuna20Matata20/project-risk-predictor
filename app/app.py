4483d915b6896a222c7bee25da8e68e283713ba8
Hakuna20Matata20 <yarik.osolihin@gmail.com>
2025-08-01 01:00:20 +0200

    Initial project structure

---
+++ app/app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Project Risk Predictor", layout="wide")
+st.title("🧠 Project Risk Predictor")
+st.write("Це базова версія. Скоро тут буде магія ризиків.")
